2fb0cce924e130e187628c37445fd304a981c2fa
topashgun <topashgun@gmail.com>
2020-08-11 01:43:22 +0530

    Updates

---
+++ precache-manifest.247c1de34552d6fd3fbb74ec98e25dc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069a992968043e4ffc8fcbfa08c330b0",
+    "url": "/star_apps_studio/index.html"
+  },
+  {
+    "revision": "0478d9504cf8d930edbc",
+    "url": "/star_apps_studio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "034c6486918bda21add9",
+    "url": "/star_apps_studio/static/css/main.a941f4c9.chunk.css"
+  },
+  {
+    "revision": "0478d9504cf8d930edbc",
+    "url": "/star_apps_studio/static/js/2.e2d422cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star_apps_studio/static/js/2.e2d422cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034c6486918bda21add9",
+    "url": "/star_apps_studio/static/js/main.ca0fcb85.chunk.js"
+  },
+  {
+    "revision": "c62fa6c77738d4fdb9fe",
+    "url": "/star_apps_studio/static/js/runtime-main.a121e02c.js"
+  }
+]);
